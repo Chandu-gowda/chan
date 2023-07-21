44fe4c8a6f4edb98faa85555ff54c4f04db3e859
Chandu-gowda <139750502+Chandu-gowda@users.noreply.github.com>
2023-07-21 14:04:59 +0530

    Create conditional_statement.java

---
+++ conditional_statement.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class conditional_statement {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num1 = sc.nextInt();
+        if (num1 > 0){
+            System.out.println("its a postive number:");
+        }
+    }
+    }
