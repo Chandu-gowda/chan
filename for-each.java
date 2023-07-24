cf7f1582c9f24174408dc864383a608abd22d9e8
Chandu-gowda <139750502+Chandu-gowda@users.noreply.github.com>
2023-07-24 14:44:52 +0530

    for-each.java

---
+++ for-each.java
@@ -0,0 +1,9 @@
+public class arrayforeach {
+        public static void main(String[] args) {
+            String[] name = {"Chandu", "gowda"};
+            for (String i : name) {
+                System.out.println(i);
+            }
+    }
+    }
+
