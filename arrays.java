6d05b3cec4b972131fe0e4c257727235e3c50e24
Chandu-gowda <139750502+Chandu-gowda@users.noreply.github.com>
2023-07-24 14:43:55 +0530

    arrays.java

---
+++ arrays.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class arrays
+{
+    public static void main(String[] args) {
+        int arrays[] = new int[5];
+        for (int i=0;i<5;i++) {
+            arrays[i] = i;
+            System.out.println(arrays[i]);
+        }
+    }
+}
