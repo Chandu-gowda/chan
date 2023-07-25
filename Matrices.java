67b92e6520b559bf92e8528ca52086b67f0cc144
Chandu-gowda <139750502+Chandu-gowda@users.noreply.github.com>
2023-07-25 13:54:15 +0530

    Matrices.java

--- Matrices.java
+++ Matrices.java
@@ -1,30 +1,20 @@
 
-import java.util.Scanner;
-public class arrayMatrices {
+public class twodarray {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter a number");
-        int rows = sc.nextInt();
-        int cols = sc.nextInt();
-        System.out.println("enter numbers");
-        int arr[][] = new int[rows][cols];
-        for (int i = 0; i < rows; i++)
+        int mat[][] = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}};
+        System.out.println(mat);
         {
-            for (int j = 0; j < cols; j++)
+            for (int i = 0; i < mat.length; i++)
             {
-                arr[i][j] = sc.nextInt();
+                for (int j = 0; j < mat[i].length; j++)
+                {
+                    System.out.print(mat[i][j] + " ");
 
-            }
-        }
-        System.out.println("Array elements; ");
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                System.out.print(arr[i][j]);
-
-            }
-            System.out.println();
+                }
+                System.out.println();
+           }
         }
 
     }
